385740d4a3d4894644397de67e52370678814c90
Alumno <Alumno@LAB1509-27>
2023-09-29 14:56:53 -0500

    Agregar archivos de proyecto.

---
+++ Lab4/Lab4/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Page2 page2= new Page2();
+            await Navigation.PushAsync(page2);
+        }
+    }
+}
